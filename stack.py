96104f8f593fd214901cf3748baf6aaa5ac9e294
Александр Бугров <bugor9682@gmail.com>
2022-08-02 12:47:54 +0300

    stack is done

--- stack.py
+++ stack.py
@@ -1,14 +1,33 @@
+class Stack:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.list_length = 0
 
-    def push_in_front(self, data):
+    def push_back(self, data):
         if self.head is None:
             self.head = Stack(data)
         else:
             first = Stack(data)
             first.next = self.head
             self.head = first
+        self.list_length += 1
+
+    def push_front(self, data):
+        if self.head is None:
+            self.head = Stack(data)
+        else:
+            end = Stack(data)
+            obj = self.head
+            while obj.next:
+                obj = obj.next
+            obj.next = end
+        self.list_length += 1
 
     def isEmpty(self):
         if self.head is None:
@@ -16,14 +35,31 @@ class LinkedList:
         else:
             return False
 
-class Stack:
+    def pop(self):
+        obj = self.head
+        if obj.next is None:
+            self.head = None
+            self.list_length = 0
+        else:
+            while obj.next.next is not None:
+                obj = obj.next
+            obj.next = None
+            self.list_length -= 1
 
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+    def peek(self):
+        obj = self.head
+        while obj.next:
+            obj = obj.next
+        return obj
+
+    def size(self):
+        return self.list_length
+
 
 new = LinkedList()
-new.push_in_front(1)
-new.push_in_front(2)
-new.push_in_front(3)
-new.push_in_front(4)
+new.push_front(1)
+new.push_front(3)
+new.push_front(4)
+new.pop()
+print(new.size())
+print(new.peek())
